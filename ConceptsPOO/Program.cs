2cd3035d604eded796fc00c11c916c377bcc4d8a
Cristian <cristianbm365@gmail.com>
2023-11-09 10:10:15 -0500

    Employee, SalaryEmployeee & CommissionEmployee

--- ConceptsPOO/Program.cs
+++ ConceptsPOO/Program.cs
@@ -3,18 +3,41 @@ using ConceptsPOO;
 
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
-
-
-try
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 11, 5));
-}
-catch (Exception error)
+    Id = 1010,
+    FirstName = "Luz",
+    LastName = "Mora",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
 {
-    Console.WriteLine(error.Message);
-}
+    Id = 2020,
+    FirstName = "Juan",
+    LastName = "Peña",
+    BirthDate = new Date(1992, 5, 23),
+    HiringDate = new Date(2023, 3, 15),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+};
+Console.WriteLine(employee2);
+
+//try
+//{
+//    Console.WriteLine(new Date(2024, 2, 29));
+//    Console.WriteLine(new Date(1974, 9, 23));
+//    Console.WriteLine(new Date(1985, 11, 5));
+//}
+//catch (Exception error)
+//{
+//    Console.WriteLine(error.Message);
+//}
 
 
 
